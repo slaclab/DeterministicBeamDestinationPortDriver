cdaa9ed6ce5f7b63d5e68e9fad958a88c27784a3
Jeremy Mock <jeremy.a.mock@gmail.com>
2023-09-21 14:04:50 -0700

    Change asyn parameter string names

--- CounterPortDriverApp/src/counter.h
+++ CounterPortDriverApp/src/counter.h
@@ -1,40 +1,27 @@
 //Counter.h 
-
-#include <iostream>
-#include <cstdint>
-#include <memory>
-#include <functional>
-#include <map>
-
+#include <epicsString.h>
 #include <iocsh.h>
+#include <iostream>
 #include <epicsExport.h>
 #include <epicsThread.h>
-#include <epicsEvent.h>
-#include <epicsTimer.h>
 #include <epicsTypes.h>
 #include <asynPortDriver.h>
 
-using namespace std;
+#define COUNTER_STRING "COUNTER"
+#define SCALAR_STRING "SCALAR"
 
-#define P_CounterString "SCOPE_COUNTER"
-#define P_CounterString2 "SCOPE_COUNTER_2"
+static const char *driverName = "Counter";
 
 class CounterDriver : public asynPortDriver {
   public:
     CounterDriver(const char *portName);
     void counterTask(void);
-  protected:
-    int P_Count;
-    int P_Count2;
+    virtual asynStatus readFloat64(asynUser *pasynUser, epicsFloat64 *value);
+
+  private:
+    int _counter;
+    int _scalar;
+    double counter;
+    double scalar;
 };
 
-//New Stuff, should be able to comment out and reset?
-/**
-class GetterDriver : public asynPortDriver {
-  public:
-    GetterDriver(const char *portName);
-    void getterTask(void);
-  protected:
-    int P_Getter;
-};
-**/
