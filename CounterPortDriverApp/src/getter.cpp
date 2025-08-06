9489643d6b9d9ff336312420802c4617cd4f5033
Lukas Ho-Koziol <lujko@slac.stanford.edu>
2025-08-06 15:23:02 -0700

    Beginning updates to flow chart location of beam

--- CounterPortDriverApp/src/getter.cpp
+++ CounterPortDriverApp/src/getter.cpp
@@ -42,6 +42,58 @@ void GetterDriver::getterTask(void)
     getDoubleParam(P_Getter, &variable);
     printf("Retrieved variable is %f\n", variable);
   }
+  int state = 0;
+  int shutter;
+  int bcs_fault;
+  int gun_off;
+  double gun_rate;
+  double hxr_permit;
+  double hard_injrate;
+  
+  if (not(shutter == 1 or bcs_fault == 0 or gun_off == 1 or gun_rate == 0 or hxr_permit == 1 or hard_injrate == 0))
+  {
+    int spectrometer_state;
+    int td_11_in;
+    int d2_in_1;
+    int d2_in_2;
+    int bykik;
+    int tdund_in;
+
+    if (spectrometer_state == 0)
+    {
+      state = 1;
+    }
+    else if (td_11_in == 2) 
+    {
+      state = 2;
+    }
+    else if (d2_in_1 == 0 or d2_in_2 == 0)
+    {
+      state = 3;
+    }
+    else if (bykik == 0)
+    {
+      state = 4;
+    }
+    else if (tdund_in == 2)
+    {
+      state = 5;
+    }
+    else 
+    {
+      state = 6;  
+    }
+    
+
+
+
+  }
+
+
+  //Set param of state
+
+
+
 }
 
 
